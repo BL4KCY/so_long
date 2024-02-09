a78bbf1d67ca7b89ad36c20368a9262bf521f203
BL4KCY <mostafarssiui@gmail.com>
2024-02-09 12:04:29 +0100

    Update exit function to accept status parameter

--- src/exit.c
+++ src/exit.c
@@ -6,17 +6,16 @@
 /*   By: melfersi <melfersi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 08:50:43 by melfersi          #+#    #+#             */
-/*   Updated: 2024/02/07 21:22:50 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/02/09 11:48:02 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	ft_exit(mlx_t *param)
+int	ft_exit(mlx_t *param, int status)
 {
 	mlx_destroy_window(param->mlx, param->win);
 	mlx_destroy_display(param->mlx);
-	ft_lstclear(&param->map, free);
 	free(param->mlx);
-	exit(EXIT_SUCCESS);
+	exit(status);
 }
