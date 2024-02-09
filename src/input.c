b6f7eb50172ba02103e7964142c164454aad634b
BL4KCY <mostafarssiui@gmail.com>
2024-02-09 19:12:08 +0100

    Update keyboard function and add set_null function

--- src/input.c
+++ src/input.c
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 08:47:29 by melfersi          #+#    #+#             */
-/*   Updated: 2024/02/09 11:54:45 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/02/09 19:00:24 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	keyboard(int keycode, mlx_t *param)
 {
 	arrow_keys(&keycode);
 	if (keycode == ESC)
-		ft_exit(param, ESCAPE_PRESS); 
+		ft_exit(param, ESCAPE_PRESS);
 	move_player(keycode, param);
 	return (0);
 }
@@ -33,4 +33,20 @@ static void	arrow_keys(int *keycode)
 		*keycode = A;
 	if (*keycode == RIGHT)
 		*keycode = D;
+}
+
+void	set_null(mlx_t *server)
+{
+	server->items.empty.x = NULL;
+	server->items.empty.y = NULL;
+	server->items.wall.x = NULL;
+	server->items.wall.y = NULL;
+	server->items.door.x = NULL;
+	server->items.door.y = NULL;
+	server->items.food.x = NULL;
+	server->items.food.y = NULL;
+	server->items.player.x = NULL;
+	server->items.player.y = NULL;
+	server->items.enemy.x = NULL;
+	server->items.enemy.y = NULL;
 }
