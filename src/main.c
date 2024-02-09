58799b8ebe1b7fc3f00969a28451233d9213575f
BL4KCY <mostafarssiui@gmail.com>
2024-02-09 12:04:45 +0100

    Refactor main.c: Update error handling and remove unnecessary code

--- src/main.c
+++ src/main.c
@@ -7,24 +7,12 @@ int main(int argc, char **argv)
 	mlx_t server;
 
 	if (argc != 2)
-	{
-		ft_putendl_fd("Error\n", STDERR_FILENO);
-		return (EXIT_FAILURE);
-	}
+		ERR("Error\nInvalid number of arguments");
 	map_parse(argv[1], &server);
 	server.mlx = mlx_init();
 	if (server.mlx == NULL)
-		return (EXIT_FAILURE);
+		ERR("Error\nmlx_init() failed\n");
 	init_game(&server);
-	server.win = mlx_new_window(server.mlx, server.width, server.height, "so_long");
-	update_empty(&server);
-	update_wall(&server);
-	if (server.win == NULL)
-	{
-		mlx_destroy_display(server.mlx);
-		free(server.mlx);
-		return (MALLOC_ERROR);
-	}
 	mlx_hook(server.win, ON_KEYDOWN, KeyPressMask, keyboard, &server);
 	mlx_hook(server.win, ON_DESTROY, KeyPressMask, ft_exit, &server);
 	mlx_loop_hook(server.mlx, render_next_frame, &server);
