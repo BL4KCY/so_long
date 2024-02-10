41dbdf6f26c3c91b3cadef9c30571cfe1804a043
BL4KCY <mostafarssiui@gmail.com>
2024-02-10 13:10:19 +0100

    Update map_parse function and add check_path function

--- src/prase_map.c
+++ src/prase_map.c
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 09:53:52 by melfersi          #+#    #+#             */
-/*   Updated: 2024/02/07 09:39:46 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/02/10 12:00:37 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int	check_line(char *line, int len, bool first, bool last)
 	static int	object[3];
 
 	i = 0;
-	if (line[0] != '1' || line[len - 1] != '1') // check first and last char
+	if (line[0] != '1' || line[len - 1] != '1')
 		return (false);
 	while (++i < len - 1)
 	{
@@ -51,7 +51,7 @@ int	check_line(char *line, int len, bool first, bool last)
 	}
 	if (last)
 	{
-		if (object[0] != 1 || object[1] != 1 || (object[2] == 0 && last)) // check if there is only one P and E and at least one C
+		if (object[0] != 1 || object[1] != 1 || (object[2] == 0 && last))
 			return (false);
 		return (object[2]);
 	}
@@ -65,7 +65,7 @@ void	map_parse(char *path, mlx_t *par)
 	char	*line;
 	char	*next_line;
 
-	(*par).map = NULL;
+	par->map = NULL;
 	fd = get_fd(path);
 	next_line = get_next_line(fd);
 	line = ft_strtrim(next_line, "\n");
@@ -74,18 +74,20 @@ void	map_parse(char *path, mlx_t *par)
 	while (line)
 	{
 		next_line = get_next_line(fd);
-		(*par).score = check_line(line, len, (*par).map == NULL, next_line == NULL);
+		par->score = check_line(line, len, par->map == NULL, next_line == NULL);
 		if (len != strlen(line) || par->score == 0)
 		{
 			ft_putendl_fd("Error\n", STDERR_FILENO);
-			ft_lstclear(&((*par).map), free);
+			ft_lstclear(&(par->map), free);
 			exit(1);
 		}
-		ft_lstadd_back(&((*par).map), ft_lstnew(line));
+		ft_lstadd_back(&(par->map), ft_lstnew(line));
 		line = ft_strtrim(next_line, "\n");
 		free(next_line);
 	}
-	close(fd);
 }
 
-
+void	check_path(mlx_t *par)
+{
+	
+}
