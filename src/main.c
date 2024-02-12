7cb4a7a6abdade6a6b90ea179ac671abae3a663b
BL4KCY <mostafarssiui@gmail.com>
2024-02-12 13:51:30 +0100

    Fix argument validation and file extension check

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 21:17:35 by melfersi          #+#    #+#             */
-/*   Updated: 2024/02/11 18:17:37 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/02/12 13:34:03 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,15 @@ int	main(int argc, char **argv)
 
 	if (argc != 2)
 		ERR("Error\nInvalid number of arguments");
+	if (ft_strncmp(&argv[1][ft_strlen(argv[1]) - 4], ".ber", 4) != 0)
+		ERR("Error\nInvalid file");
 	map_parse(argv[1], &server);
 	server.mlx = mlx_init();
 	if (server.mlx == NULL)
 		ERR("Error\nmlx_init() failed\n");
 	init_game(&server);
-	mlx_hook(server.win, ON_KEYDOWN, KeyPressMask, keyboard, &server);
-	mlx_hook(server.win, ON_DESTROY, KeyPressMask, ft_exit, &server);
+	mlx_hook(server.win, ON_KEYDOWN, KEYPRESSMASK, keyboard, &server);
+	mlx_hook(server.win, ON_DESTROY, KEYPRESSMASK, ft_exit, &server);
 	mlx_loop_hook(server.mlx, render_next_frame, &server);
 	mlx_loop(server.mlx);
 }
