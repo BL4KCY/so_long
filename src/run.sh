584b1195838604b0febe3f59cd2b15013b7bf56d
BL4KCY <mostafarssiui@gmail.com>
2024-02-13 18:35:21 +0100

    Remove unnecessary run.sh script

--- src/run.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-cc -fsanitize=leak prase_map.c ../libft/libft.a -I../includes -I../libft/includes && clear && ./a.out /nfs/homes/melfersi/Cursus/mlx/maps/valid/map-square.ber
-sleep 1
-bash run.sh
